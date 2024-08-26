f8033eef1aab212ee7f97c1b04e18b28d0af4b63
Kartik Rao <143627728+KartikkRao@users.noreply.github.com>
2024-08-26 14:34:06 +0530

    Create glue_job.py

---
+++ glue_job.py
@@ -0,0 +1,41 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+  
+sc = SparkContext.getOrCreate()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+from pyspark.sql.functions import *
+from datetime import datetime 
+from awsglue.dynamicframe import DynamicFrame
+import boto3
+s3_path = "s3://ecomm-data203/raw_data/"
+
+df = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(s3_path)
+df.printSchema()
+df.show(1)
+df = df.drop("category_code")
+df = df.withColumn("brand", when(col("brand").isNull(), "other").otherwise(col("brand"))).show(10)
+output_path = f"s3://ecomm-data203/transformed_data/ecomm_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+
+df.write.mode("overwrite").option("header" , "true").format("csv").save(output_path)
+s3 = boto3.client('s3')
+
+bucket_name = 'ecomm-data203'
+prefix = 'raw_data/'
+
+# List objects in the bucket with the specified prefix
+response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+
+#print(response)
+
+if 'Contents' in response:
+    for obj in response['Contents']:
+        if obj['Key'] != 'raw_data/':
+            s3.delete_object(Bucket=bucket_name, Key=obj['Key'])
+
+job.commit()
